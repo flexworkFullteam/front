3e89f9c69110344de3a3c44c48274ad2308364a2
Stefano Garbocci <stefgarbocci@gmail.com>
2023-10-12 16:20:55 -0300

    feat(): Terminado de rutas preliminares

--- src/project/routes/ProjectRoute.jsx
+++ src/project/routes/ProjectRoute.jsx
@@ -1,12 +1,19 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
+import { HomePage } from "../pages/HomePage/HomePage"
+import { SearchPage } from "../pages/SearchPage.jsx/SearchPage"
+import { DetailPage } from "../pages/DetailPage/DetailPage"
 
 
 export const ProjectRoute = () => {
   return (
     <Routes>
-        <Route path="/*" element={<div>ProjectRoute</div>} />
+        <Route path="/home" element={<HomePage />} />
 
+        <Route path="/search" element={<SearchPage />} />
 
+        <Route path="/detail" element={<DetailPage />} />
+
+        <Route path="/*" element={ <Navigate to="/home" /> } />
     </Routes>
   )
 }
