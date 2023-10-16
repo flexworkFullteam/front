c3a0f00fae79009e79972033d8ec85ffd9425e36
Stefano Garbocci <stefgarbocci@gmail.com>
2023-10-16 15:51:35 -0300

    feat(SearchPage, DetailPage): Continuacion de logica

--- src/project/pages/DetailPage/DetailPage.jsx
+++ src/project/pages/DetailPage/DetailPage.jsx
@@ -1,20 +1,24 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
-import { Box, Button, Grid, Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, Grid, Typography } from "@mui/material";
 
 export const DetailPage = () => {
 
   const {id} = useParams();
   const {status} = useSelector((state) => state.auth);
-  const dispacth = useDispatch();
+  const dispatch = useDispatch();
+
+  const {allProjects} = useSelector((state) => state.project); //* Esto no deberia ir realmente
+
+  const filteredProject = allProjects.find((project) => project.id === +id);
+  
+  console.log(filteredProject);
 
   useEffect(() => {
     // dispacth(getProjectById(id)) //! falta crear la accion
   }, [])
   
-  
-
   return (
     <Grid container>
       <Grid item xs={12} display='flex' justifyContent='space-between' sx={{ boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.2)' }} >
@@ -33,11 +37,39 @@ export const DetailPage = () => {
 
           </Box>
       </Grid>
+
+
       <Grid item xs={8}>
-       
+        <Card sx={{ mb: "1rem", ":hover": {cursor: "pointer"} }}>
+          <CardContent>
+            <Typography variant="h5" component="h2">
+              {filteredProject.titulo}
+            </Typography>
+            <Typography color="textSecondary">
+              {filteredProject.date} - {filteredProject.level} - {filteredProject.workload}
+            </Typography>
+            <Typography variant="body2" component="p">
+              {filteredProject.descripcion}
+            </Typography>
+          </CardContent>
+        </Card>
       </Grid>
+
+
       <Grid item xs={4}>
-       
+      <Card sx={{ mb: "1rem", ":hover": {cursor: "pointer"} }}>
+          <CardContent>
+            <Typography variant="h5" component="h2">
+              {filteredProject.titulo}
+            </Typography>
+            <Typography color="textSecondary">
+              {filteredProject.date} - {filteredProject.level} - {filteredProject.workload}
+            </Typography>
+            <Typography variant="body2" component="p">
+              {filteredProject.descripcion}
+            </Typography>
+          </CardContent>
+        </Card>
       </Grid>
     </Grid>
   )
