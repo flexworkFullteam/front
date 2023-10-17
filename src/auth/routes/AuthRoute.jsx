fff412dce15b5662877d9a9b4a2be1600451f200
Karmen <carmen.mendez.aguirre@gmail.com>
2023-10-17 00:28:58 -0600

    feat(Account): registerUser, conponentes company/profesional

--- src/auth/routes/AuthRoute.jsx
+++ src/auth/routes/AuthRoute.jsx
@@ -1,8 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import { LoginProfePage } from "../pages/LoginProfePage";
 import { LoginCompanyPage } from "../pages/LoginCompanyPage";
-import { RegisterProfePage } from "../pages/RegisterProfePage";
-import { RegisterCompanyPage } from "../pages/RegisterCompanyPage";
+import { RegisterUserPage } from "../pages/RegisterUserPage";
+// import { RegisterProfePage } from "../pages/RegisterProfePage";
+// import { RegisterCompanyPage } from "../pages/RegisterCompanyPage";
 
 export const AuthRoute = () => {
   return (
@@ -10,8 +11,9 @@ export const AuthRoute = () => {
       <Route path="/loginProf" element={<LoginProfePage />} />
       <Route path="/loginComp" element={<LoginCompanyPage />} />
       {/* <Route path="login" element={<h1>LoginPage</h1> } /> */}
-      <Route path="/registerProf" element={<RegisterProfePage />} />
-      <Route path="/registerComp" element={<RegisterCompanyPage />} />
+      <Route path="/registerUser" element={<RegisterUserPage />} />
+      {/* <Route path="/registerProf" element={<RegisterProfePage />} />
+      <Route path="/registerComp" element={<RegisterCompanyPage />} /> */}
       {/* <Route path="register" element={<h1>RegisterPage</h1>} /> */}
       {/* <Route path="/*" element={<Navigate to="/auth/login" />} /> */}
     </Routes>
