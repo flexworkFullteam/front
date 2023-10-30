bd9233c8193a0bca972bae47c54d6c4c05d93d35
Stefano Garbocci <stefgarbocci@gmail.com>
2023-10-30 20:34:03 -0300

    feat(): Paymen

--- src/project/components/Payment/PaymentConfirmation.jsx
+++ src/project/components/Payment/PaymentConfirmation.jsx
@@ -1,7 +1,20 @@
 import { Box, Button, Typography } from "@mui/material"
+import { CheckRounded as CheckRoundedIcon, CloseRounded as CloseRoundedIcon } from "@mui/icons-material/";
 
 
-export const PaymentConfirmation = () => {
+export const PaymentConfirmation = ({onClose, id, title, salary, user }) => {
+
+
+    const AcceptPayment = async() => {
+        await startPayment({
+        title: title,
+        unit_price: salary,
+        currency_id: "PEN",
+        from: user.id,
+        project: id
+        });
+    }
+
   return (
     <Box
         sx={{
@@ -12,6 +25,16 @@ export const PaymentConfirmation = () => {
             height: "100%",
         }}
     >
+        <div 
+        // className={styles.topContainer}
+        >
+            <CloseRoundedIcon  onClick={onClose} 
+            // className={styles.closeButton}
+            />
+            <Typography variant='h4' sx={{ mb: "1rem", textAlign: "center" }}>
+            Postulantes
+            </Typography>
+        </div>
         <Typography>
                 Queremos recordarle que al aceptar al profesional, se le cobrará el monto de la comisión por el servicio.
         </Typography>
@@ -24,10 +47,10 @@ export const PaymentConfirmation = () => {
                 ¿Desea continuar?
         </Typography>
 
-        <Button>
+        <Button onClick={onClose}>
             Cancelar
         </Button>
-        <Button>
+        <Button onClick={AcceptPayment}>
             Aceptar
         </Button>
     </Box>
