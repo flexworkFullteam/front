be64d7ea1b1a8bed66673cf8eccebc0aeb46f841
Stefano Garbocci <stefgarbocci@gmail.com>
2023-11-01 17:26:12 -0300

    fix(): Alerts

--- src/hooks/useProjectStore.js
+++ src/hooks/useProjectStore.js
@@ -16,13 +16,13 @@ export const useProjectStore = () => {
       dispatch(onGetAllProjects(data));
     } catch (error) {
       console.error(error);
-      Swal.fire({
-        position: 'top-center',
-        icon: 'error',
-        title: error.message,
-        showConfirmButton: false,
-        timer: 1500
-      })
+      // Swal.fire({
+      //   position: 'top-center',
+      //   icon: 'error',
+      //   title: error.message,
+      //   showConfirmButton: false,
+      //   timer: 1500
+      // })
     }
   };
 
@@ -56,24 +56,31 @@ export const useProjectStore = () => {
       dispatch(onSetActiveEvent(data));
     } catch (error) {
       console.error(error.message);
-      Swal.fire({
-        position: 'top-center',
-        icon: 'error',
-        title: error.message,
-        showConfirmButton: false,
-        timer: 1500
-      })
+      // Swal.fire({
+      //   position: 'top-center',
+      //   icon: 'error',
+      //   title: error.message,
+      //   showConfirmButton: false,
+      //   timer: 1500
+      // })
     }
   };
 
   const deleteProject = async (id) => {
     try {
       await projectAPI.delete(`/project/${id}`);
+      Swal.fire({
+        position: 'top-center',
+        icon: 'success',
+        title: 'Proyecto eliminado',
+        showConfirmButton: false,
+        timer: 1500
+      })
     } catch (error) {
       Swal.fire({
         position: 'top-center',
         icon: 'error',
-        title: error.message,
+        title: "Error al eliminar el proyecto",
         showConfirmButton: false,
         timer: 1500
       })
